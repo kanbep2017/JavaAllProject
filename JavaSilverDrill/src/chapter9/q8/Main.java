9dbff7baa4f233fe62b0312d3f490f54173fda56
kanbep2017 <kanbep2017@gmail.com>
2025-07-31 17:01:05 +0900

    JavaSilver11_第9章問8 アウトプット

---
+++ JavaSilverDrill/src/chapter9/q8/Main.java
@@ -0,0 +1,16 @@
+package chapter9.q8;
+
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		ArrayList list = new ArrayList<>();
+		list.add("A");
+		list.add(10);
+		list.add('B');
+		for (Object obj : list) {
+			System.out.print(obj);
+		}
+	}
+}
