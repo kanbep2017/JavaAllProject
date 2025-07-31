ab70f90c6c5bb0c4f2e474d480c24a8d1a769043
kanbep2017 <kanbep2017@gmail.com>
2025-07-31 13:55:24 +0900

    JavaSilver11_第9章問3 アウトプット

---
+++ JavaSilverDrill/src/chapter9/q3/Main.java
@@ -0,0 +1,15 @@
+package chapter9.q3;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(new Integer[] { 1, 2, 3 });
+		list.sort((a, b) -> -a.compareTo(b));
+		for (Integer num : list) {
+			System.out.println(num);
+		}
+	}
+}
