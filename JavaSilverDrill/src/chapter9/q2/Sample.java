8dcc4a84f682f4660f02292a4756f115d3fd80a1
kanbep2017 <kanbep2017@gmail.com>
2025-07-31 13:39:42 +0900

    JavaSilver11_第9章問2 アウトプット

---
+++ JavaSilverDrill/src/chapter9/q2/Sample.java
@@ -0,0 +1,21 @@
+package chapter9.q2;
+
+public class Sample {
+	private int id;
+	public String name;
+
+	public Sample(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
