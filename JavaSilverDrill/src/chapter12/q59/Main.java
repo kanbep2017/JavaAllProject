2b6f15a10030a08ea00074f193fff36efe304c30
kanbep2017 <kanbep2017@gmail.com>
2025-08-11 17:41:53 +0900

    JavaSilver11_第12章問59 アウトプット

---
+++ JavaSilverDrill/src/chapter12/q59/Main.java
@@ -0,0 +1,12 @@
+package chapter12.q59;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 2;
+		System.out.println(a + b + " = (a + b) = " + a + b);
+		// 出力結果は "6 = (a + b) = 42" です。
+	}
+
+}
