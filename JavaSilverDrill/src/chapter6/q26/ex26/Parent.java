685fa206f8a7754a655f9def2b2b0f89c2ec776c
kanbep2017 <kanbep2017@gmail.com>
2025-09-27 16:36:44 +0900

    JavaSilver11_第6章問26 アウトプット

---
+++ JavaSilverDrill/src/chapter6/q26/ex26/Parent.java
@@ -0,0 +1,6 @@
+package chapter6.q26.ex26;
+
+public class Parent {
+	public static int num = 10;
+	// publicとstaticを外すとコンパイルエラーになる。
+}
---
+++ JavaSilverDrill/src/chapter6/q26/other/Child.java
@@ -0,0 +1,10 @@
+package chapter6.q26.other;
+
+import chapter6.q26.ex26.Parent;
+
+public class Child extends Parent {
+	public static void main(String[] args) {
+		System.out.println(num); // 同じパッケージに入れないとコンパイルエラーになる
+	}
+
+}
